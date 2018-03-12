48676df08383f43c59a02f376dc951690c90618d
Tan Le <tanld.ce@gmail.com>
2018-03-12 23:33:31 +0700

    FLow control
    1. Condition: if/else, switch case, ternary operator.
    2. Loop:

---
+++ 1-intro/operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main()
+{
+	using namespace std;
+	/* Increment and Decrement Operators
+	int a = 8, b = 8, c = 8, d = 8;
+	cout << "a++ = " << a++ << endl;
+	cout << " after increment: " << a << endl;
+	cout << "++b = " << ++b << endl;
+	cout << "c-- = " << c-- << endl;
+	cout << "--d = " << --d << endl;
+	*/
+	/* sizeof() */
+	int i = 6;
+	int j;
+	char c;
+	float f;
+	double d;
+	cout << "size of integer variable i : " << sizeof(i) << endl;
+	cout << "size of integer variable j : " << sizeof(j) << endl;
+	cout << "size of character variable c : " << sizeof(c) << endl;
+	cout << "size of float variable f : " << sizeof(f) << endl;
+	cout << "size of double variable d : " << sizeof(d) << endl;
+
+	return 0;
+}
