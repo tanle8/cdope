fe4f1bd1f87033d2972da3c1fa9dd987ad4a4065
tanld <tanld.ce@gmail.com>
2018-04-21 20:31:29 +0700

    pointer pratice #6

--- 5-pointers/5-practice/5p-l1-6.cpp
+++ 5-pointers/5-practice/5p-l1-6.cpp
@@ -7,16 +7,44 @@
 
 using namespace std;
 
-void compareNum()
+void compareNum(double *n1, double *n2, double *n3)
 {
-    
+    if (*n1 > *n2 && *n1 > *n3) {
+        cout << *n1 <<" is the largest and ";
+        if (*n2 > *n3) {
+            cout << *n3 << " is the smallest among three numbers" << endl;
+        } else {
+            cout << *n2 << " is the smallest among three numbers" << endl;
+        }
+
+    } else if (*n2 > *n1 && *n2 > *n3) {
+        cout << *n2 << " is the largest and ";
+        if (*n1 > *n3) {
+            cout << *n3 << " is the smallest among three numbers" << endl;
+        } else {
+            cout << *n1 << " is the smallest among three numbers" << endl;
+        }
+        
+    } else {
+        cout << *n3 << " is the largest and ";
+        if (*n1 > *n2) {
+            cout << *n2 << " is the smallest among three numbers" << endl;
+        } else {
+            cout << *n1 << " is the smallest among three numbers" << endl;
+        }
+    }
 
 }
 
 int main()
 {
-    float n1, n2, n3;
-    float *p1, *p2, *p3;
+    double n1, n2, n3;
+    cout << "Enter three numbers you want to compare:" << endl;
+    cin >> n1 >> n2 >> n3;
+    cout << "Three numbers are: " << n1 << " " << n2 << " " << n3 << endl;
+
+    compareNum(&n1, &n2, &n3);
+
 
     return 0;
 }
